15192f342c7d9b11f20063a495497e80cb8422da
Montaser Al-joubeh <mon-taser-t-j@hotmail.com>
2023-08-13 15:16:11 +0300

    upload project to github

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'pages/quiz_page.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const QuizPage(title: 'Quiz Page'),
+    );
+  }
+}
+
+
