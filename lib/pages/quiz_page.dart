219d06a58824a2b797fe337683d5f4cfdf579e7d
David Jenner <david.jenner@live.com>
2024-09-01 12:48:33 +0100

    added tech questions

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -8,53 +8,34 @@ class QuizPage extends StatefulWidget {
   @override
   State<QuizPage> createState() => _QuizPageState();
 }
+
 class _QuizPageState extends State<QuizPage> {
   int currentQuestionIndex = 0;
   bool answerSelected = false;
   bool quizFinished = false; // Track if the quiz is finished
+  
 
   static List<Map<String, dynamic>> questionsWithAnswers = [
     {
-      'question': 'What is the capital of Palestine?',
+      'question': 'What does CPU stand for?',
       'answers': [
-        {'text': 'Nablus', 'image': 'assets/images/nablus.jpg'},
-        {'text': 'Ramallah', 'image': 'assets/images/rammallah.jpg'},
-        {'text': 'Gaza', 'image': 'assets/images/gaza.jpg'},
-        {'text': 'Jerusalem', 'image': 'assets/images/jarusalem.jpeg'},
-      ],
-      'correctAnswer': 3,
-    },
-    {
-      'question': 'What Palestine is bordered by to the east?',
-      'answers': [
-        {'text': 'Lebanon', 'image': 'assets/images/flag-lbn.png'},
-        {'text': 'Jordan', 'image': 'assets/images/flag-jor.png'},
-        {'text': 'Syria', 'image': 'assets/images/flag-syr.png'},
-        {'text': 'Egypt', 'image': 'assets/images/flag-egy.png'},
-      ],
-      'correctAnswer': 1,
-    },
-    {
-      'question': 'What is the Palestinian city known for the Al-Aqsa mosque and the Church of the Resurrection?',
-      'answers': [
-        {'text': 'Ramallah', 'image': 'assets/images/rammallah.jpg'},
-        {'text': 'Gaza', 'image': 'assets/images/gaza.jpg'},
-        {'text': 'Jerusalem', 'image': 'assets/images/jarusalem.jpeg'},
-        {'text': 'Bethlehem', 'image': 'assets/images/bethlahem.jpg'},
-      ],
-      'correctAnswer': 2,
-    },
-    {
-      'question': 'What was Jaffa Famous?',
-      'answers': [
-        {'text': 'Orange', 'image': 'assets/images/orange.jpg'},
-        {'text': 'Apple', 'image': 'assets/images/apple.jpg'},
-        {'text': 'Guava', 'image': 'assets/images/guava.jpg'},
-        {'text': 'Grapes', 'image': 'assets/images/grapes.jpg'},
+        {'text': 'Central Processing Unit', 'image': 'assets/images/CPU.png'},
+        {'text': 'Central Programming Unit', 'image': 'assets/images/CPU.png'},
+        {'text': 'Computer Personal Unit', 'image': 'assets/images/CPU.png'},
+        {'text': 'Central Processor Unit', 'image': 'assets/images/CPU.png'},
       ],
       'correctAnswer': 0,
     },
-
+    {
+      'question': 'Which language is primarily used for Android app development?',
+      'answers': [
+        {'text': 'Swift', 'image': 'assets/images/android.jpg'},
+        {'text': 'JavaScript', 'image': 'assets/images/android.jpg'},
+        {'text': 'Kotlin', 'image': 'assets/images/android.jpg'},
+        {'text': 'Python', 'image': 'assets/images/android.jpg'},
+      ],
+      'correctAnswer': 2,
+    },
   ];
 
   List<String?> selectedAnswers = List.filled(questionsWithAnswers.length, null);
@@ -100,12 +81,14 @@ class _QuizPageState extends State<QuizPage> {
     int score = 0;
     for (int i = 0; i < selectedAnswers.length; i++) {
       if (selectedAnswers[i] != null &&
-          selectedAnswers[i] == questionsWithAnswers[i]['answers'][questionsWithAnswers[i]['correctAnswer']]['text']) {
+          selectedAnswers[i] ==
+              questionsWithAnswers[i]['answers'][questionsWithAnswers[i]['correctAnswer']]['text']) {
         score++;
       }
     }
     return score;
   }
+
   @override
   Widget build(BuildContext context) {
     if (quizFinished) {
@@ -139,8 +122,7 @@ class _QuizPageState extends State<QuizPage> {
                     onPressed: restartQuiz,
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white
-                    ),
+                        foregroundColor: Colors.white),
                     child: const Text(
                       "Restart Quiz",
                       style: TextStyle(fontSize: 18),
@@ -174,20 +156,27 @@ class _QuizPageState extends State<QuizPage> {
                 ),
                 const Text(
                   'Answer the question and gets points',
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal,color: Colors.grey),
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey),
                   textAlign: TextAlign.start,
                 ),
                 const SizedBox(height: 20.0),
                 Text(
                   'Question ${currentQuestionIndex + 1} of ${questionsWithAnswers.length}',
-                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold,color: Colors.deepPurple),
+                  style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple),
                   textAlign: TextAlign.start,
                 ),
                 LinearProgressIndicator(
                   value: answerSelected
                       ? calculateProgress()
                       : currentQuestionIndex / questionsWithAnswers.length,
-                color: Colors.deepPurple,),
+                  color: Colors.deepPurple,
+                ),
                 const SizedBox(height: 50.0),
                 for (final answer in answers)
                   Padding(
@@ -205,7 +194,10 @@ class _QuizPageState extends State<QuizPage> {
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.white,
                           foregroundColor: selectedAnswers[currentQuestionIndex] == answer['text'] ? Colors.green : null,
-                          disabledForegroundColor: selectedAnswers[currentQuestionIndex] == answer['text'] ? Colors.green : null,
+                          side: selectedAnswers[currentQuestionIndex] == answer['text']
+                              ? const BorderSide(color: Colors.purple, width: 2.0)
+                              : null,
+                          elevation: selectedAnswers[currentQuestionIndex] == answer['text'] ? 4.0 : 0.0,
                         ),
                         child: Row(
                           children: [
@@ -238,8 +230,7 @@ class _QuizPageState extends State<QuizPage> {
                     onPressed: goToNextQuestion,
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white
-                    ),
+                        foregroundColor: Colors.white),
                     child: const Text(
                       "Next Question",
                       style: TextStyle(fontSize: 18),
